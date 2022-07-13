f73394542edefff428b5847151697c465212d2d9
marinkus <marinkus@gmail.com>
2022-07-13 17:29:13 +0300

    forEach

--- 001/app.js
+++ 001/app.js
@@ -26,3 +26,21 @@ namas.push('Naujas gyventojas atsiranda po visu');
 namas.unshift('Naujas gyventojas atsiras pries visus');
 
 console.log(namas);
+
+const cats = ['Murka', 'Rainis', 'Pilkis', 'Ausyte', 'Soriukas', 'Upe'];
+
+console.log(cats[rand(0, cats.length - 1)]);
+
+for (let number of mas) {
+    console.log(number);
+}
+console.log('~~~~~~~~~~~');
+
+for (let i = 0; i < mas.length; i++) {
+    console.log(mas[i]);
+}
+
+// Improvement
+console.log('++++++++++++++');
+
+mas.forEach(n => console.log(n));
