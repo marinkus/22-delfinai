bc437ca68996785f08505f3405eff259e96b53f9
marinkus <marinkus@gmail.com>
2022-07-15 08:31:21 +0300

    testas

---
+++ 002/testas.js
@@ -0,0 +1,45 @@
+console.clear();
+
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+const girls = ['Brigita', 'Edita', 'Deimantė', 'Justė', 'Ona', 'Bronė', 'Ramunė', 'Kaštonė', 'Meilutė'];
+
+// 1
+const cats = [...Array(10)].map(_ => rand(2, 12));
+const owners = [...Array(10)].map(_ => girls[rand(0, 8)])
+
+console.log(cats, owners);
+// 2
+
+console.log(cats.length);
+console.log(cats);
+console.log(cats.filter(n => !(n % 3)));
+
+// 3
+
+owners.unshift('Nausėda');
+console.log(owners);
+
+// 4
+let catOwners = [];
+for (let i = 0; i < owners.length; i++) {
+    catOwners.push(`${owners[i]} has ${cats[i]} cats}`)
+}
+
+// cats masyvo ilgis nera lygus owners ilgiui, todel kai kur bus undefined
+console.log(catOwners);
+
+// 5
+
+for (let name of owners) {
+     for (let i = 0; i < owners.length; i++) {
+        if (name === owners[i + 1]) {
+            console.log(name);
+            break;
+        }
+        break;
+     }
+}
