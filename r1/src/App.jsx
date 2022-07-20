b5e5d7e78a36ef3784c269ef6da1b73383393541
marinkus <marinkus@gmail.com>
2022-07-20 14:19:39 +0300

    list

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -3,11 +3,18 @@ import Kosmosas from './Components/005/Kosmosas.jsx';
 
 const ms = {color: 'crimson', backgroundColor: 'white'};
 
+const arr = ['Pilkis', 'Murkis', 'Gagarinas'];
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>MAIN CONTENT</h1>
+
+        {
+          arr.map((n, i) => <h2 key={i}>{n}</h2>)
+        }
         <Kosmosas spalva='magenta' krastas='40px'></Kosmosas>
         <Kosmosas manoStilius={ms}></Kosmosas>
       </header>
