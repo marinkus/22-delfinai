d05ba2fcf8f59c1fc4c07ac3b8c0da45b4069d5e
marinkus <marinkus@gmail.com>
2022-07-25 08:46:41 +0300

    HW #2.1

--- r1/src/Components/006/HomeworkState21.jsx
+++ r1/src/Components/006/HomeworkState21.jsx
@@ -5,33 +5,35 @@ import { useState } from "react";
 
 function HomeworkState21() {
    
-    const [ hardwood, setHardwood ] = useState([]);
-    const addAzuolas = () => {setHardwood(s => [...s, 'Azuolas'])};
-    const addBerzas = () => {setHardwood(s => [...s, 'Berzas'])};
-    const addUosis = () => {setHardwood(s => [...s, 'Uosis'])};
+    const [ azuolas, setAzuolas ] = useState([]);
+    const [ berzas, setBerzas ] = useState([]);
+    const [ uosis, setUosis ] = useState([]);
+    const addAzuolas = () => {setAzuolas(s => [...s, 'Azuolas'])};
+    const addBerzas = () => {setBerzas(s => [...s, 'Berzas'])};
+    const addUosis = () => {setUosis(s => [...s, 'Uosis'])};
 
     return (
         <>
         <h2>Homework 2.1</h2>
-            <div style={{display: 'flex', width: '33%', flexWrap: "wrap", backgroundColor: 'green'}}>
-            <button onClick={addAzuolas} style={{width: '45px', height: '25px'}}>Add azuolas</button>
+        <div style={{display: 'flex', width: '100%', height: '800px', backgroundColor: 'green', boxSizing: 'border-box'}}>
+            <div style={{width: '30%px', backgroundColor: 'green'}}>
+            <button onClick={addAzuolas} style={{width: '45px', height: '40px'}}>Add azuolas</button>
             {
-                hardwood.map((a, i) => <div style={{margin: '20px 20px', width: '160px', height: '160px', border: '1px solid black'}} key={i}>{a}</div>)
+                azuolas.map((a, i) => <div style={{margin: '20px 20px', width: '100px', height: '100px', border: '1px solid black'}} key={i}>{a}</div>)
             }
             </div>
-            <div style={{display: 'flex', width: '33%', flexWrap: "wrap", backgroundColor: 'green'}}>
-            <button onClick={addBerzas} style={{width: '45px', height: '25px'}}>Add berzas</button>
+            <div style={{width: '30%', backgroundColor: 'green'}}>
+            <button onClick={addBerzas} style={{width: '45px', height: '40px'}}>Add berzas</button>
                 {
-                    hardwood.map((a, i) => <div style={{margin: '20px 20px', width: '160px', height: '160px', border: '1px solid red'}} key={i}>{a}</div>)
+                    berzas.map((a, i) => <div style={{margin: '20px 20px', width: '100px', height: '100px', border: '1px solid red'}} key={i}>{a}</div>)
                 }
             </div>
-            <div style={{display: 'flex', width: '33%', flexWrap: "wrap", backgroundColor: 'green'}}>
-            <button onClick={addUosis} style={{width: '45px', height: '25px'}}>Add uosis</button>
+            <div style={{width: '30%', backgroundColor: 'green'}}>
+            <button onClick={addUosis} style={{width: '45px', height: '40px'}}>Add uosis</button>
             {
-                hardwood.map((a, i) => <div style={{margin: '20px 20px', width: '160px', height: '160px', border: '1px solid red'}} key={i}>{a}</div>)
+                uosis.map((a, i) => <div style={{margin: '20px 20px', width: '100px', height: '100px', border: '1px solid red'}} key={i}>{a}</div>)
             }
             </div>
-        <div>
         </div>
         </>
     )
