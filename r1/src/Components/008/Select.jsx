aa8227461e115b9c0aa20c3cbbb2560cf88801cd
marinkus <marinkus@gmail.com>
2022-07-26 12:47:51 +0300

    Select

--- r1/src/Components/008/Select.jsx
+++ r1/src/Components/008/Select.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 function Select() {
 
     const [select, setSelect] = useState(1000);
+    const [selected, setSelected] = useState(1000);
 
     const handleInput = e => {
         setSelect(e.target.value);
@@ -11,13 +12,14 @@ function Select() {
     return (
         <>
         <fieldset>
-            <legend>Select</legend>
+            <legend>Selected: {selected}</legend>
             <select value={select} onChange={handleInput}>
                 <option value="1">One</option>
                 <option value="2">Two</option>
                 <option value="3">Three</option>
                 <option value="1000">Thousand</option>
             </select>
+            <button onClick={() => setSelected(select)}>Button</button>
         </fieldset>
         </>
     )
