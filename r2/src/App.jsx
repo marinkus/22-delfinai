093c0e3aa564bd8748d4b50bd8aa4bc0bad4cc4f
marinkus <marinkus@gmail.com>
2022-07-27 14:26:04 +0300

    crud

--- r2/src/App.jsx
+++ r2/src/App.jsx
@@ -1,19 +1,33 @@
 import './bootstrap.css';
-import './App.css';
+import './App.scss';
 import Create from './Components/Create';
+import AnimalsContext from './Components/AnimalsContext';
+
+const animalsTypes = [
+  { id: 1, type: 'Antis' },
+  { id: 2, type: 'Avis' },
+  { id: 3, type: 'Antilopas' },
+  { id: 4, type: 'Bebras' },
+  { id: 5, type: 'Briedis' },
+  { id: 6, type: 'Barsukas' }
+];
 
 function App() {
   return (
-    <div className='container'>
-      <div className="row">
-        <div className="col-4">
-          <Create></Create>
-        </div>
-        <div className="col-8">
-          One of three columns
+    <AnimalsContext.Provider value={{
+      animalsTypes
+    }}>
+      <div className='container'>
+        <div className="row">
+          <div className="col-4">
+            <Create></Create>
+          </div>
+          <div className="col-8">
+            One of three columns
+          </div>
         </div>
       </div>
-    </div>
+    </AnimalsContext.Provider>
   );
 }
 
--- r2/src/Components/Create.jsx
+++ r2/src/Components/Create.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import { useContext } from "react";
+import AnimalsContext from "./AnimalsContext";
+
 function Create() {
 
+    const { animalsTypes } = useContext(AnimalsContext);
 
-    return <h2>create</h2>
+    const [type, setType] = useState(5);
+
+
+    return (
+        <div className="card m-4">
+            <h5 className="card-header">Add new animal</h5>
+            <div className="card-body">
+                <div className="form-group">
+                    <label>Our animal types</label>
+                    <select className="form-control" value={type} onChange={e => setType(e.target.value)}>
+                        {
+                            animalsTypes.map(a => <option key={a.id} value={a.id}>{a.type}</option>)
+                        }
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label>How much is the {animalsTypes.find(a => a.id === Number.parseInt(type)).type}</label>
+                    <input type="text" className="form-control" />
+                    <small className='form-text text-muted'>Please enter your animal wieght in kg</small>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Create;
