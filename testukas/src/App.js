e46827177efd653d663c06cc39b2672c41ddd5ed
marinkus <marinkus@gmail.com>
2022-08-26 13:56:28 +0300

    #3 done

--- testukas/src/App.js
+++ testukas/src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 import Animals from "./components/animals/Animals";
 import Pokemons from "./components/Pokemons";
+import Knygos from "./components/Knygos";
 
 export default function App() {
   return (
     <div className="App">
-      <Animals></Animals>
-      <Pokemons></Pokemons>
+      {/* <Animals></Animals>
+      <Pokemons></Pokemons> */}
+      <Knygos></Knygos>
     </div>
   );
 }
---
+++ testukas/src/components/Knygos.jsx
@@ -0,0 +1,53 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+function Knygos() {
+
+    const [knygos, setKnygos] = useState([]);
+    const [types, setTypes] = useState([]);
+    const [loading, setLoading] = useState(true);
+    // const [loading2, setLoading2] = useState(true);
+
+    useEffect(() => {
+
+        fetch("https://in3.dev/knygos/")
+            .then((response) => response.json())
+            .then(res => {
+                setKnygos(res);
+                setLoading(false);
+            });
+
+
+    }, []);
+    useEffect(() => {
+
+        fetch("https://in3.dev/knygos/types/")
+            .then((response) => response.json())
+            .then(res => {
+                setTypes(res);
+                setLoading(false);
+            });
+
+
+    }, []);
+
+    if (loading) {
+        return <div className='title'>Loading, please wait...</div>
+    } else {
+        return (
+            <div className="App">
+                    <h1>Books:</h1>
+                    <div className='container'>
+                        {
+                            knygos.map(knyga => <figure key={knyga.type} className="box"><img src={knyga.img} alt={knyga.title} /><figcaption
+                           className="text" >{knyga.title} by {knyga.author}</figcaption>
+                           <p className="price">Price: {knyga.price}$</p>{
+                            types.map(type => type.id === knyga.id ? <p>{type.title}</p> : null)
+                            }</figure>)
+                        }
+                    </div>
+            </div>
+        );
+    };
+}
+export default Knygos;
