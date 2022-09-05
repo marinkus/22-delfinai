658c5e7015b9cd1066b286e4863f620d713ee0b9
marinkus <marinkus@gmail.com>
2022-09-05 13:47:18 +0300

    headers

--- zoo/src/App.jsx
+++ zoo/src/App.jsx
@@ -10,17 +10,27 @@ import axios from 'axios';
 function App() {
 
   const [animals, setAnimals] = useState(null);
+  const [createData, setCreateData] = useState(null);
+
 
   useEffect(() => {
     axios.get('http://animals.zoo/react/list')
     .then(res => setAnimals(res.data));
   }, []);
 
+  useEffect(() => {
+    if (null === createData) {
+      return;
+    }
+    axios.post('http://animals.zoo/react/list', createData)
+    .then(res => console.log(res))
+  }, [createData])
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-5">
-          <Create />
+          <Create setCreateData={setCreateData} />
         </div>
         <div className="col-7">
           <List list={animals} />
--- zoo/src/Components/Create.jsx
+++ zoo/src/Components/Create.jsx
@@ -1,6 +1,49 @@
-function Create() {
+import { useState } from "react";
+
+function Create({ setCreateData }) {
+
+    const [type, setType] = useState('');
+    const [weight, setWeight] = useState('');
+    const [tail, setTail] = useState(false);
+
+    const add = () => {
+        const data = {
+            type,
+            weight,
+            tail: tail ? 1 : 0
+        }
+        setCreateData(data);
+    }
+
     return (
-            <h1>Create</h1>
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-12">
+                    <div className="card m-4">
+                        <div className="card-header">
+                            <h2>New Animal</h2>
+                        </div>
+                        <div className="card-body">
+                            <div className="form-group">
+                                <label>Type</label>
+                                <input type="text" className="form-control" value={type} onChange={e => setType(e.target.value)} />
+                                <small className="form-text text-muted">Animal type</small>
+                            </div>
+                            <div className="form-group">
+                                <label>Weight</label>
+                                <input type="text" className="form-control" value={weight} onChange={e => setWeight(e.target.value)} />
+                                <small className="form-text text-muted">Animal weight in kg</small>
+                            </div>
+                            <div className="form-group form-check">
+                                <input type="checkbox" className="form-check-input" value="1" checked={tail} onChange={() => setTail(t => !t)} />
+                                <label className="form-check-label">Has tail?</label>
+                            </div>
+                            <button type="button" className="btn btn-primary mt-5" onClick={add}>Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
