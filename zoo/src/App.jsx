40359c516916594fb7c2d8cfe2cc04b0655f89c8
marinkus <marinkus@gmail.com>
2022-09-05 13:53:25 +0300

    last update

--- zoo/src/App.jsx
+++ zoo/src/App.jsx
@@ -11,19 +11,24 @@ function App() {
 
   const [animals, setAnimals] = useState(null);
   const [createData, setCreateData] = useState(null);
+  const [lastUpdate, setLastUpdate] = useState(Date.now());
 
 
   useEffect(() => {
     axios.get('http://animals.zoo/react/list')
     .then(res => setAnimals(res.data));
-  }, []);
+  }, [lastUpdate]);
 
   useEffect(() => {
     if (null === createData) {
       return;
     }
     axios.post('http://animals.zoo/react/list', createData)
-    .then(res => console.log(res))
+    .then(res => {
+      setLastUpdate(Date.now());
+      console.log(res)
+})
+      
   }, [createData])
 
   return (
